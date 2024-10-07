29c122418eb5ae36d92fb41943906efdf5e1933d
Joakim Olofsson <joakim.olofsson@iths.se>
2024-10-07 18:39:19 +0200

    One exercise done

--- Repetition/Program.cs
+++ Repetition/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person[] persons = new Person[3];
+
+            Console.WriteLine("Type three names!");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Name {i+1}:");
+                persons[i] = new Person()
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+
+            foreach(Person person in persons)
+            {
+                Console.WriteLine($"Hello! My name is {person.ToString()}.");
+            }
+        }
+
+        class Person
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
         }
     }
 }
